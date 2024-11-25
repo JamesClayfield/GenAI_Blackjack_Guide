64c896202ace9581a7a044bcfe7b35e25f189ace
James Dieter Clayfield <james.clayfield@hotmail.ch>
2024-11-25 16:19:20 +0100

    Add files via upload

--- vision.py
+++ vision.py
@@ -49,12 +49,17 @@ Task = """
 You have been first shown a black jack strategy guide in CSV format.
 Each row represents an optimal move based on the dealer's up card and the user's hand.
 
+The format of each row is one of the following:
+
+"Dealer's up card: W. Your hand (face value): Z. Optimal play: Hit, Stay, Double Down, Split (depends which)."
+
+"Dealer's up card: W. Your cards: X, Y. Optimal play: Hit, Stay, Double Down, Split (depends which)."
+
 You are also shown an image. The 2 or more cards next to each other that are face up are your hand. This is the hand you are meant to evaluate.
 The pair of cards where one is face up and the other is face down are the dealer's cards.
 
 King (K), Queen (Q), Jack (J) are equal to 10.
 Read the number from the top left of the card to identify the face value.
-
 
 Tell me which cards you see in the dealer's hand. Also tell me the cards you are holding.
 
